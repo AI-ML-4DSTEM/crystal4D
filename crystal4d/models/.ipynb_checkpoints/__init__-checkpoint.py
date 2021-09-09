3c5dd7185d2f19a07e37231e250ebfaa8f075334
Joydeep Munshi <63680361+jmunshi1990@users.noreply.github.com>
2021-09-09 18:17:20 -0400

    Delete crystal4d/models/.ipynb_checkpoints directory

--- crystal4d/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from .complexUnet import *
-from .complexSpecPoolUnet import *
-from .complexUnetMultiHead import *
-from .spectralPoolUnet import *
-from .spectralUnet import *
-from .unet import *
